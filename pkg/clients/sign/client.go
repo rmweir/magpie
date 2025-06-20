60726c3937afd5467005b716ce99e83738268249
Ricardo Weir <ricardo.weir@loft.sh>
2025-06-20 15:50:50 -0700

    Sign configmap workings

--- pkg/clients/sign/client.go
+++ pkg/clients/sign/client.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 
 	"github.com/loft-sh/magpie/pkg/signing"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -27,10 +30,10 @@ func (c Client) Get(ctx context.Context, key client.ObjectKey, obj client.Object
 	}
 
 	signature := obj.GetAnnotations()[signatureAnnotation]
+	revertValuesFunc := removeNonComparableValues(obj)
+	defer revertValuesFunc()
 
-	delete(obj.GetAnnotations(), signatureAnnotation)
-
-	err = signing.Verify(obj, []byte(signature))
+	err = signing.Verify(obj, signature)
 	if err != nil {
 		return fmt.Errorf("signature verification failed: %w", err)
 	}
@@ -44,7 +47,7 @@ func (c Client) Create(ctx context.Context, obj client.Object, opts ...client.Cr
 		return fmt.Errorf("failed to sign object: %w", err)
 	}
 
-	obj.SetAnnotations(map[string]string{signatureAnnotation: string(signature)})
+	obj.SetAnnotations(map[string]string{signatureAnnotation: signature})
 
 	err = c.Client.Create(ctx, obj, opts...)
 	if err != nil {
@@ -54,13 +57,15 @@ func (c Client) Create(ctx context.Context, obj client.Object, opts ...client.Cr
 }
 
 func (c Client) Update(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
-	delete(obj.GetAnnotations(), signatureAnnotation)
+	revertValuesFunc := removeNonComparableValues(obj)
 
 	signature, err := signing.Sign(obj)
 	if err != nil {
+		revertValuesFunc()
 		return fmt.Errorf("failed to sign object: %w", err)
 	}
 
+	revertValuesFunc()
 	obj.SetAnnotations(map[string]string{signatureAnnotation: string(signature)})
 
 	err = c.Client.Update(ctx, obj, opts...)
@@ -68,4 +73,35 @@ func (c Client) Update(ctx context.Context, obj client.Object, opts ...client.Up
 		return fmt.Errorf("failed to update object: %w", err)
 	}
 	return nil
+}
+
+func removeNonComparableValues(obj client.Object) func() {
+	signature := obj.GetAnnotations()[signatureAnnotation]
+	resourceVersion := obj.GetResourceVersion()
+	creationTimestamp := obj.GetCreationTimestamp()
+	uid := obj.GetUID()
+	gvk := obj.GetObjectKind().GroupVersionKind()
+	managedFields := obj.GetManagedFields()
+
+	delete(obj.GetAnnotations(), signatureAnnotation)
+	obj.SetResourceVersion("")
+	obj.SetCreationTimestamp(v1.Time{})
+	obj.SetUID("")
+	obj.SetManagedFields(nil)
+
+	_, ok := obj.(*unstructured.Unstructured)
+	if !ok {
+		// with normal types (any type that is not unstructured), setting the GVK is optional. Usually created will be
+		// missing it and updates will contain it. We just remove it here as it is not needed.
+		obj.GetObjectKind().SetGroupVersionKind(schema.GroupVersionKind{})
+	}
+
+	return func() {
+		obj.SetAnnotations(map[string]string{signatureAnnotation: signature})
+		obj.SetResourceVersion(resourceVersion)
+		obj.SetCreationTimestamp(creationTimestamp)
+		obj.SetUID(uid)
+		obj.SetManagedFields(managedFields)
+		obj.GetObjectKind().SetGroupVersionKind(gvk)
+	}
 }
--- pkg/signing/sign.go
+++ pkg/signing/sign.go
@@ -22,39 +22,44 @@ func init() {
 	publicKey = os.Getenv("TEST_READ_KEY")
 }
 
-func Sign(obj any) ([]byte, error) {
+func Sign(obj any) (string, error) {
 	bytes, err := json.Marshal(obj)
 	if err != nil {
-		return nil, fmt.Errorf("failed to marshal object: %w", err)
+		return "", fmt.Errorf("failed to marshal object: %w", err)
 	}
 
 	decodedKey, err := base64.StdEncoding.DecodeString(privateKey)
 	if err != nil {
-		return nil, fmt.Errorf("failed to decode private key: %w", err)
+		return "", fmt.Errorf("failed to decode private key: %w", err)
 	}
 
 	b, _ := pem.Decode(decodedKey)
 
 	key, err := x509.ParsePKCS8PrivateKey(b.Bytes)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse private key: %w", err)
+		return "", fmt.Errorf("failed to parse private key: %w", err)
 	}
 
 	rsaKey, ok := key.(*rsa.PrivateKey)
 	if !ok {
-		return nil, errors.New("failed to parse public key, must be RSA public key")
+		return "", errors.New("failed to parse public key, must be RSA public key")
 	}
 
 	sum256Hash := sha256.Sum256(bytes)
 	signature, err := rsa.SignPKCS1v15(nil, rsaKey, crypto.SHA256, sum256Hash[:])
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	return signature, nil
+	return base64.StdEncoding.EncodeToString(signature), nil
 }
 
-func Verify(obj any, signature []byte) error {
+func Verify(obj any, signature string) error {
+	decodedSignature, err := base64.StdEncoding.DecodeString(signature)
+	if err != nil {
+		return fmt.Errorf("failed to decode signature: %w", err)
+	}
+
 	bytes, err := json.Marshal(obj)
 	if err != nil {
 		return fmt.Errorf("failed to marshal object: %w", err)
@@ -78,7 +83,7 @@ func Verify(obj any, signature []byte) error {
 	}
 
 	sum256Hash := sha256.Sum256(bytes)
-	err = rsa.VerifyPKCS1v15(rsaKey, crypto.SHA256, sum256Hash[:], signature)
+	err = rsa.VerifyPKCS1v15(rsaKey, crypto.SHA256, sum256Hash[:], decodedSignature)
 	if err != nil {
 		return fmt.Errorf("failed to verify signature: %w", err)
 	}
